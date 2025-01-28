bd73353536a734e7ae32cf668a3947ff072b7b9f
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-01-28 17:36:05 +0530

    groupby method of Map + Async js exercise

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var x = 5;
+const fruits = [
+    {name:"apples", quantity:300},
+    {name:"bananas", quantity:500},
+    {name:"oranges", quantity:200},
+    {name:"kiwi", quantity:150}
+];
 
-function fun(){
-    var x = 10;
+// Callback function to select low volumes 
+function myCallback({ quantity }) {
+    return quantity > 200 ? "ok" : "low";
 }
 
-let object = {name:"gautam"}
+// Group by ok and low
+const result = Map.groupBy(fruits, myCallback);
 
-document.write(object.__proto__);
+// Display Results
+let text ="These fruits are Ok: <br>";
+for (let x of result.get("ok")) {
+    text += x.name + " " + x.quantity + "<br>";
+}
+text += "<br>These fruits are low: <br>";
+for (let x of result.get("low")) {
+    text += x.name + " " + x.quantity + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+
+console.log(result);
