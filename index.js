34805eb5ee2664870947b2774ea11da1c2b42a4d
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-01-30 14:09:38 +0530

    additional info in index.html

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const fruits = [
-    {name:"apples", quantity:300},
-    {name:"bananas", quantity:500},
-    {name:"oranges", quantity:200},
-    {name:"kiwi", quantity:150}
-];
+// const fruits = [
+//     {name:"apples", quantity:300},
+//     {name:"bananas", quantity:500},
+//     {name:"oranges", quantity:200},
+//     {name:"kiwi", quantity:150}
+// ];
 
-// Callback function to select low volumes 
-function myCallback({ quantity }) {
-    return quantity > 200 ? "ok" : "low";
+// // Callback function to select low volumes 
+// function myCallback({ quantity }) {
+//     return quantity > 200 ? "ok" : "low";
+// }
+
+// // Group by ok and low
+// const result = Map.groupBy(fruits, myCallback);
+
+// // Display Results
+// let text ="These fruits are Ok: <br>";
+// for (let x of result.get("ok")) {
+//     text += x.name + " " + x.quantity + "<br>";
+// }
+// text += "<br>These fruits are low: <br>";
+// for (let x of result.get("low")) {
+//     text += x.name + " " + x.quantity + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// console.log(result);
+var p = 7;
+function f(){
+    console.log(p);
+    var p = 14;
 }
+f();
 
-// Group by ok and low
-const result = Map.groupBy(fruits, myCallback);
-
-// Display Results
-let text ="These fruits are Ok: <br>";
-for (let x of result.get("ok")) {
-    text += x.name + " " + x.quantity + "<br>";
-}
-text += "<br>These fruits are low: <br>";
-for (let x of result.get("low")) {
-    text += x.name + " " + x.quantity + "<br>";
-}
-document.getElementById("demo").innerHTML = text;
-
-console.log(result);
+console.log(typeof typeof 7)   // typeof number => string
+console.log(typeof ([] + []))  // empty string("")
